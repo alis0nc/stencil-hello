cfbd3daa68a2250c4f3251d9bd086ee7882d8e7d
Alison Chan <alison.chan@companycam.com>
2022-02-16 12:00:58 -0500

    referral level by count

--- src/components.d.ts
+++ src/components.d.ts
@@ -20,6 +20,12 @@ export namespace Components {
          */
         "middle": string;
     }
+    interface ReferralLevel {
+        /**
+          * Referral level
+         */
+        "referralCount": number;
+    }
 }
 declare global {
     interface HTMLMyComponentElement extends Components.MyComponent, HTMLStencilElement {
@@ -28,8 +34,15 @@ declare global {
         prototype: HTMLMyComponentElement;
         new (): HTMLMyComponentElement;
     };
+    interface HTMLReferralLevelElement extends Components.ReferralLevel, HTMLStencilElement {
+    }
+    var HTMLReferralLevelElement: {
+        prototype: HTMLReferralLevelElement;
+        new (): HTMLReferralLevelElement;
+    };
     interface HTMLElementTagNameMap {
         "my-component": HTMLMyComponentElement;
+        "referral-level": HTMLReferralLevelElement;
     }
 }
 declare namespace LocalJSX {
@@ -47,8 +60,15 @@ declare namespace LocalJSX {
          */
         "middle"?: string;
     }
+    interface ReferralLevel {
+        /**
+          * Referral level
+         */
+        "referralCount"?: number;
+    }
     interface IntrinsicElements {
         "my-component": MyComponent;
+        "referral-level": ReferralLevel;
     }
 }
 export { LocalJSX as JSX };
@@ -56,6 +76,7 @@ declare module "@stencil/core" {
     export namespace JSX {
         interface IntrinsicElements {
             "my-component": LocalJSX.MyComponent & JSXBase.HTMLAttributes<HTMLMyComponentElement>;
+            "referral-level": LocalJSX.ReferralLevel & JSXBase.HTMLAttributes<HTMLReferralLevelElement>;
         }
     }
 }
---
+++ src/components/referral-level/referral-level.tsx
@@ -0,0 +1,30 @@
+import { Component, Prop, h } from '@stencil/core';
+
+@Component({
+  tag: 'referral-level',
+  styleUrl: 'referral-level.css',
+  shadow: true,
+})
+export class ReferralLevel {
+  /**
+   * Referral level
+   */
+  @Prop() referralCount: number;
+
+  private getLevelName(count: number): string {
+    switch (count) {
+      case 1:
+        return 'one of them';
+      case 2:
+        return 'two of them';
+      case 3:
+        return 'three of them';
+      default:
+        return 'too many of them';
+    }
+  }
+
+  render() {
+    return <div>{this.getLevelName(this.referralCount)}</div>;
+  }
+}
